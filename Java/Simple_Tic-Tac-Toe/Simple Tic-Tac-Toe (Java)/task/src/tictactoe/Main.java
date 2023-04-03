33af0ebbea0e9abc2511efa97697c7a7aae2065b
7802229J <nicolas.bouchet@sncf.fr>
2023-04-03 22:01:33 +0200

    Stage 2/5: The user is the gamemaster - DONE

--- Java/Simple_Tic-Tac-Toe/Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Java/Simple_Tic-Tac-Toe/Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -1,30 +1,33 @@
 package tictactoe;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner scanner = new Scanner(System.in);
-        String[][] gameArray = new String[3][3];
-        for (int i = 0; i < gameArray.length; i++) {
-            for (int j = 0; j < gameArray[i].length; j++) {
-                String symbol = String.valueOf(scanner.next().charAt(i));
-                if ((Objects.equals("O", symbol)) || (Objects.equals("X", symbol)) || (Objects.equals("_", symbol))) {
-                    gameArray[i][j] = symbol;
-                } else {
-                    System.out.println("Error, only 'X', 'O', or'_' accepted");
-                    break;
-                }
-            }
-        }
-        // TODO create a regex to test the input
 
-        for (int i = 0; i < gameArray.length; i++) {
-            for (int j = 0; j < gameArray[i].length; j++) {
-                System.out.println(gameArray[i][j]);
-            }
+        String playerInput = "";
+        while (!playerInput.matches("^[XO_]{9}$")) {
+            playerInput = scanner.nextLine();
         }
+        char[] symbol = playerInput.toCharArray();
+        char[][] gameArray = new char[3][3];
+
+        for (int i = 0; i < symbol.length; i++) {
+            int row = i / 3;
+            int column = i % 3;
+            gameArray[row][column] = symbol[i];
+        }
+
+        System.out.println("---------");
+        for (int i = 0; i < gameArray.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < gameArray[i].length; j++) {
+                System.out.print(gameArray[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
